2416634804018fdbdfebd9f9be1fd5779e4ff1dd
Tomeek92 <98541483+Tomeek92@users.noreply.github.com>
2024-05-02 14:10:31 +0200

    changes in views add new element in JS

---
+++ Bootstrap/wwwroot/js/startInTopList.js
@@ -0,0 +1,32 @@
+﻿let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        let panel = this.nextElementSibling;
+        let isActive = this.classList.contains("active");
+
+        let allPanels = document.querySelectorAll(".panel");
+        allPanels.forEach(function (item) {
+            if (item !== panel) {
+                item.style.maxHeight = null;
+                item.previousElementSibling.classList.remove("active");
+            }
+        });
+
+        this.classList.toggle("active");
+        if (isActive) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+            // Przewiń stronę tylko gdy lista jest rozwinięta
+            let accordionTopOffset = document.querySelector(".accordion").offsetTop;
+            window.scrollTo({ top: accordionTopOffset, behavior: 'smooth' })
+        }
+    });
+}
+function restartVideo() {
+    var video = document.getElementById("myVideo");
+    video.currentTime = 0;
+    video.play();
+}

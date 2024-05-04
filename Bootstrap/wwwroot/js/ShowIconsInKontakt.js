8f88748b13a39f3bd2835242f750a1dc3f5b733f
Tomeek92 <98541483+Tomeek92@users.noreply.github.com>
2024-05-04 17:03:33 +0200

    add View and style to Kontakt

---
+++ Bootstrap/wwwroot/js/ShowIconsInKontakt.js
@@ -0,0 +1,7 @@
+﻿const icons = document.querySelectorAll('.socialMediaa');
+icons.forEach((icon, index) => {
+    setTimeout(() => {
+        icon.style.opacity = '1';
+    }, (index + 1) * 1000);
+});
+
